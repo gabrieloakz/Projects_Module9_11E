2cafd753e2c2c3ca3de4a1f3e97c0acb92523709
Gabriel Ferreira Carvalho <53023@aebatalha.ccems.pt>
2023-09-29 11:13:55 +0100

    Console_App0

---
+++ Console_Apps/Console_App0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp0
+{
+    internal class Progam
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Como se chama? ");
+            //criar uma variável para o nome a seguir
+            string NomePropio = Console.ReadLine();
+            Console.WriteLine("Bem vindo(a) {0}.", NomePropio);
+        }
+    }
+}
