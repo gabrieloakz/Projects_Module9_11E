2a92ffdeffe8066c5cfb929fc7dfb3ec82be8823
Gabriel Ferreira Carvalho <53023@aebatalha.ccems.pt>
2023-10-03 17:32:17 +0100

    atividades

---
+++ Ficha_de_Trabalho1/Exercício 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Code made by Gabriel Carvalho 11E TGPSI AEB
+
+//1.Elabora um programa que escreva na consola:
+//a.O teu nome e apelido
+//b. O Nome da escola
+//c. O Nome da disciplina
+
+using System;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gabriel Ferrreira Carvalho");
+            Console.WriteLine("Escola Secundária da Batalha");
+            Console.WriteLine("Progamação de Sistemas Informáticos");
+        }
+    }
+}
---
+++ Ficha_de_Trabalho1/Exercício 2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Code made by Gabriel Carvalho 11E TGPSI AEB
+
+//2.Elabora um programa que peça dois números inteiros ao utilizador e efetue as 
+//seguintes operações: 
+//a.Soma
+//b.Multiplicação
+//c.Divisão
+//d.Subtração
+//e.Média
+//f.O resto da divisão
+
+using System;
+
+namespace Exercício2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o 1º número:");
+            int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o 2º número:");
+            int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("A soma é: {0}", primeiroNumero + segundoNumero);
+            Console.WriteLine("A Multiplicação é: {0}", primeiroNumero * segundoNumero);
+            Console.WriteLine("A Divisão é: {0}", primeiroNumero / segundoNumero);
+            Console.WriteLine("A Subtração é: {0}", primeiroNumero - segundoNumero);
+            Console.WriteLine("A Média é: {0}", (primeiroNumero + segundoNumero)/2);
+            Console.WriteLine("O resto da divisão é: {0}", primeiroNumero % segundoNumero);
+        }
+    }
+}
