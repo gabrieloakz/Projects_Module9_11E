8e03870bc0de4b11004cba3d24137b52487dcd7e
Gabriel Ferreira Carvalho <53023@aebatalha.ccems.pt>
2023-10-04 15:22:24 +0100

    começo segunda questao ficha

--- Ficha de Trabalho 2/Ex1/Exercicio1/Program.cs
+++ Ficha de Trabalho 2/Ex1/Exercicio1/Program.cs
@@ -1,14 +1,12 @@
-﻿//1.Utilize o Objeto “Math” e apresente os resultados das seguintes alíneas:
-//1.1.Resultado de um número elevado à potência;
-//1.2.Arredonde um número do tipo “double”;
-//1.3.Retorne o valor de PI;
-//1.4.Raiz quadrada de um número;
-//1.5.Retorne o menor valor acima de um número inserido;
-//1.6.Retorne o maior valor acima de um número inserido;
-//1.7.Retorne o valor mínimo de dois números inseridos;
-//1.8.Retorne o valor máximo de dois números inseridos;
+﻿//2.Declare duas variáveis string (x , y) à sua escolha e utilize as seguintes propriedades: 
+//2.1.Primeiro caractere da primeira variável “x”: Substring(0, 1)
+//2.2.Último caractere da variável “y”: Substring(__.Length - 1, 1))
+//2.3.Todos menos o primeiro caracter da variável “x”: Substring(1))
+//2.4.O terceiro elemento “x”: Substring(3, 1) ou seja 4º caracter
+//2.5. Os três primeiros elementos da variável “y”: Substring(0, 3))
+//2.6.Os três últimos elementos de “y”: Substring(____.Length - 3, 3))
 
-// Code made by Gabriel Carvalho 11E TGPSI AEB
+//Code made by Gabriel Carvalho 11E TGPSI AEB
 
 using System;
 using System.Collections.Generic;
@@ -16,32 +14,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercicio1
+namespace Exercicio2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t-----Exercicio Objeto Math-----");
+            Console.WriteLine("\t-----Exercicio Objeto String-----");
 
-            Console.WriteLine("\n");
-            
-            Console.WriteLine("\tPotenciação: {0}", Math.Pow(10.5, 3)); //Base 10.5 elevado a 3
-            
-            Console.WriteLine("\tArredondamento: {0}", Math.Round(9.98d, 1)); //Arredonda 9.98 double para 10
-            
-            Console.WriteLine("\tPI π: {0}", Math.PI); //PI é apenas o PI: Neste caso não é um método é um valor fixo
-            
-            Console.WriteLine("\tRaiz Quadrada: {0}", Math.Sqrt(25d)); //Sqrtr: Raiz quadrada de um número
-            
-            Console.WriteLine("\tMenor Valor de um Número(2.5): {0}", Math.Floor(2.5)); //Retorna o menor valor acima de um número inserido
-            
-            Console.WriteLine("\tMaior Valor de um Número(2.5): {0}", Math.Ceiling(2.5)); //Retorna o maior valor acima de um número inserido
-            
-            Console.WriteLine("\tValor mínimo de dois números(2,6): {0}", Math.Min(2, 6)); //Retorna o valor mínimo de dois números inseridos
-            
-            Console.WriteLine("\tValor máximo de dois números(2,6): {0}", Math.Max(2, 6)); //Retorna o valor máximo de dois números inseridos
-            
+            Console.WriteLine("\n")
+
+            string x = "arroz";
+            string y = "feijao";
+
+            // Utiliza a propriedade Substring para obter o primeiro caractere de 'x'
+            string primeiroCaractereX = x.Substring(0, 1);
+
+            // Exiba o resultado
+            Console.WriteLine($"O primeiro caractere de 'x' é: {primeiroCaractereX}");
+
+
+
             Console.ReadKey(); //Pausa a execução sem fechar direto
         }
     }
